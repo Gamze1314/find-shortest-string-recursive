787f60d6a53abf19f63f8b9e95539a98cd2f7d18
gamzekandara <gamzekandara@gmail.com>
2025-04-25 15:41:23 -0400

    Add recursive find shortest string func

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,5 +1,19 @@
 function findShortestStringRecursive(arr) {
   // type your code here
+  // base case
+  if (arrLength === 0) {
+    return null;
+  }
+  if (arrLength === 1) {
+    return arr[0];
+  }
+  // recursive case
+  // store the resulting value in a variable called result
+  // call function with a smaller array.
+  const result = findShortestStringRecursive(arr.slice(1));
+
+  return arr[0].length <= result.length ? arr[0] : result;
+
 }
 
 if (require.main === module) {
@@ -21,4 +35,19 @@ if (require.main === module) {
 module.exports = findShortestStringRecursive;
 
 // Please add your pseudocode to this file
+// find the shortest string in the array
+// array length min is  1.
+// if the array is empty return null
+// if the array has only one element return that element
+// if the array has more than one element
+// compare the first element with the rest of the elements
+// repeat the process until the shortest string is found.
+
+
+
 // And a written explanation of your solution
+//i can use recursion to find the shortest string in the array, i can repeat the process for each element if the length of the array is greater than 1.
+// base case is when the array has only one element, i can return that element..
+// i can repeat the process for each element in the array and compare the length of the first element with the rest of the elements.
+// if the first element is shorter than the rest of the elements, i can return the first element.
+// if the first element is longer than the rest of the elements, i can return the shortest element in the rest of the elements.
